92fb2070249fe20cc4b25b731e0b80e8403063a7
jieglee <callmezie23@gmail.com>
2025-09-02 12:25:24 +0700

    deploy

---
+++ script.js
@@ -0,0 +1,9 @@
+/*==================== typed js ====================*/
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+    
+        var typed = new Typed ('.yawr',{
+            strings : ['Frontend Developer', 'Ui/UX Designer'],
+            typeSpeed : 100,
+            backSpeed : 100,
+            loop : true,
+        });
